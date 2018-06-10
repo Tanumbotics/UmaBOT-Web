b93e92cf3994f4a06698d06c50f081a9a64702d6
Marc Anthony Reyes <marcreyesmedia@gmail.com>
2018-06-11 02:11:53 +0800

    Import separation cleanup
    Signed-off-by: Marc Anthony Reyes <marcreyesmedia@gmail.com>

--- tensorflow_web/views.py
+++ tensorflow_web/views.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from django.core.files.storage import FileSystemStorage
 from django.core.files.temp import NamedTemporaryFile
+
 from .forms import UploadFileForm
 from PIL import Image
 from weasyprint import HTML
